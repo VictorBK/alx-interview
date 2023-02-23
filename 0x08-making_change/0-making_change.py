e1bad90eee941a4e423d85aba72ba9122dc7eebd
VictorBK <victorbrian4@gmail.com>
2023-02-23 12:43:52 +0300

    Change comes from within mend

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-""" makeChange function """
+"""Making change module
+"""
+
 
 def makeChange(coins, total):
-    if total == 0:
+    """Finds the minimum number of coins needed to make change
+    """
+    if (total <= 0):
         return 0
-    coins.sort(reverse=True)
-    count = 0
+    coin_arr = [float('inf') for i in range(total + 1)]
+    coin_arr[0] = 0
     for coin in coins:
-        while total - coin >= 0:
-            total -= coin
-            count += 1
-        if total == 0:
-            return count
-    return -1
+        for x in range(len(coin_arr)):
+            if coin <= x:
+                coin_arr[x] = min(coin_arr[x], 1 + coin_arr[x - coin])
+    return coin_arr[total] if coin_arr[total] != float('inf') else -1
